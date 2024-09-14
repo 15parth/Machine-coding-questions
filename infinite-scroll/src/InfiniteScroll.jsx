8413909ad5926caa14f5d1bb32999bd34cbafd97
Parth Bhardwaj <pbhardwaj@datopic.com>
2024-09-14 19:19:44 +0530

    fixed issues regarding the api call

--- infinite-scroll/src/InfiniteScroll.jsx
+++ infinite-scroll/src/InfiniteScroll.jsx
@@ -3,48 +3,67 @@ import './App.css'
 
 const InfiniteScroll = () => {
     const [page, setPage] = useState(1);
-    const [data , setData] = useState([]);
-
+    const [data, setData] = useState([]);
+    const [loading ,setLoading] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
-        //   setLoading(true);
-          try {
-            const response = await fetch(
-              `https://picsum.photos/v2/list?page=1&limit=3`
-            );
-            const result = await response.json();
-            setData((oldData) => [...oldData, ...result]);
-          } catch (error) {
-            console.error("Error fetching data:", error);
-          }
-        //    finally {
-        //     // setLoading(false);
-        //   }
+              setLoading(true);
+            try {
+                const response = await fetch(
+                    `https://picsum.photos/v2/list?page=${page}&limit=3`
+                );
+                const result = await response.json();
+                setData((oldData) => [...oldData, ...result]);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+               finally {
+                setLoading(false);
+              }
         };
-    
+
         fetchData();
-      }, []);
+    }, [page]);
 
 
-    useEffect(()=>{
+    useEffect(() => {
+     const handleScroll =()=>{
+        const nearBottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight -100;
 
-    },[])
+        if(nearBottom && !loading){
+            setPage((prevPage)=> prevPage +1)
+        }
 
-    console.log(data)
+     }
 
-  return (
-    <div>
-      {data?.map((item ,index)=>{
-        return (
-            <div className="container" id={index}>
-            <span >this is the author name {item?.author}</span>
-            <span >this is id {item?.id}</span>
-            </div>
-        )
-      })}
-    </div>
-  )
+     window.addEventListener('scroll' , handleScroll);
+
+    }, [loading])
+
+    // console.log(data)
+
+    return (
+        <div>
+            {data?.map((item) => {
+                return (
+                   <div id={item?.id}>
+                     <div className="container" id={item?.id}>
+                        <span >this is id {item?.id }</span>
+                        <span >this is the author name {item?.author}</span>
+                        <img
+                            className="image-post"
+                            key={item.id}
+                            src={item.download_url}
+                            alt="Post"
+                        />
+                    </div>
+                    {loading && <p>Loading...</p>}
+                   </div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default InfiniteScroll
